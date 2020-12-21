597b033884f9143583ecffc8db8a44b38280e788
liuhu <1649471814@qq.com>
2020-12-21 16:49:42 +0800

    代码生成器服务构建

---
+++ book-generatorr/src/main/java/com/motang/generator/GeneratorApplication.java
@@ -0,0 +1,16 @@
+package com.motang.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description 代码生成器启动类
+ * @author liuhu
+ * @Date 2020/12/21 16:30
+ */
+@SpringBootApplication
+public class GeneratorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorApplication.class,args);
+    }
+}
